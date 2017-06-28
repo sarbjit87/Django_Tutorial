06b8ee791f745fdd153889eed19725d314e8bdb6
sarbjit87 <sarbjit@Sarbjits-MacBook-Air.local>
2017-06-27 21:21:15 -0400

    Custom admin, model manager

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,3 +7,16 @@ from accounts.models import UserProfile
 # Register your models here.
 admin.site.register(UserProfile)
 #admin.site.site_header = 'Administration'
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','user_info','city','website','phone')
+
+    def user_info(self,obj):
+        return obj.description
+
+    def get_queryset(self,request):
+        queryset = super(UserProfileAdmin,self).get_queryset(request)
+        queryset = queryset.order_by('phone')
+        return queryset
+
+    user_info.short_description = 'Info'
